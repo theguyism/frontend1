42c7153b7a68d8a4e14ab29b4d0e6c9bc46961a0
astralaria <pawut.j@gmail.com>
2022-08-17 23:17:28 +0700

    Fix Path

---
+++ page/Home.js
@@ -0,0 +1,67 @@
+import {
+  TouchableOpacity,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Button,
+} from "react-native";
+
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>Sainsbury's</Text>
+      <Text style={styles.sub_header}>Reserve & Delivery</Text>
+      <Image style={styles.main_img} source={require("./pic1.png")} />
+      <TouchableOpacity
+        style={styles.loginScreenButton}
+        //onPress={() => navigate('HomeScreen')}
+        underlayColor="#C8263A"
+      >
+        <Text style={styles.loginText}>Get Started</Text>
+      </TouchableOpacity>
+      <StatusBar style="auto" />
+    </View>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FE7224",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  header: {
+    fontSize: 36,
+    color: "white",
+  },
+  sub_header: {
+    fontSize: 16,
+    color: "#E3AEAE",
+    marginTop: 10,
+  },
+  main_img: {
+    width: 153,
+    height: 138,
+    borderRadius: 57,
+    marginTop: 30,
+    marginBottom: 45,
+  },
+
+  loginScreenButton: {
+    marginRight: 40,
+    marginLeft: 40,
+    marginTop: 10,
+    paddingTop: 10,
+    paddingBottom: 10,
+    backgroundColor: "#C8263A",
+    borderWidth: 1,
+  },
+
+  loginText: {
+    color: "white",
+  },
+});
