0ebd5c5b50a791b63e88b1808bfac6752e40b24e
astralaria <pawut.j@gmail.com>
2022-08-17 23:15:35 +0700

    Fix Problem And and git igore

--- App.js
+++ App.js
@@ -7,20 +7,21 @@ import {
   Image,
   Button,
 } from "react-native";
-//import { NativeScreenNavigationContainer } from "react-native-screens";
-//import { createNativeStackNavigator } from "@react-navigation/native-stack";
-//import { StackActions } from "@react-navigation/native";
+import { NativeScreenNavigationContainer } from "react-native-screens";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StackActions } from "@react-navigation/native";
+import { NavigationContainer } from "@react-navigation/native";
 //import HomeScreen from "./page/Home.js";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NativeScreenNavigationContainer>
+    <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
       </Stack.Navigator>
-    </NativeScreenNavigationContainer>
+    </NavigationContainer>
   );
 }
 
