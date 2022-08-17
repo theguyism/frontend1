22bfb09380cbd3b990097f0cbf3c9d1b238aef36
theguyism <95016197+theguyism@users.noreply.github.com>
2022-08-17 16:46:42 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,82 @@
+import { StatusBar } from "expo-status-bar";
+import {
+  TouchableOpacity,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Button,
+} from "react-native";
+//import { NativeScreenNavigationContainer } from "react-native-screens";
+//import { createNativeStackNavigator } from "@react-navigation/native-stack";
+//import { StackActions } from "@react-navigation/native";
+//import HomeScreen from "./page/Home.js";
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NativeScreenNavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NativeScreenNavigationContainer>
+  );
+}
+
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>Sainsbury's</Text>
+      <Text style={styles.sub_header}>Reserve & Delivery</Text>
+      <Image style={styles.main_img} source={require("./pic1.png")} />
+      <TouchableOpacity
+        style={styles.loginScreenButton}
+        //onPress={() => navigate('HomeScreen')}
+        underlayColor="#C8263A"
+      >
+        <Text style={styles.loginText}>Get Started</Text>
+      </TouchableOpacity>
+      <StatusBar style="auto" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FE7224",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  header: {
+    fontSize: 36,
+    color: "white",
+  },
+  sub_header: {
+    fontSize: 16,
+    color: "#E3AEAE",
+    marginTop: 10,
+  },
+  main_img: {
+    width: 153,
+    height: 138,
+    borderRadius: 57,
+    marginTop: 30,
+    marginBottom: 45,
+  },
+
+  loginScreenButton: {
+    marginRight: 40,
+    marginLeft: 40,
+    marginTop: 10,
+    paddingTop: 10,
+    paddingBottom: 10,
+    backgroundColor: "#C8263A",
+    borderWidth: 1,
+  },
+
+  loginText: {
+    color: "white",
+  },
+});
